5b216e284db63404077d00d28a86fb4cbb94cf59
Oron Kastel-Mizrahi <oron.kastel@gmail.com>
2019-12-23 17:35:28 +0200

    delete lib directory

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-declare const chalk: any;
-declare const clear: any;
-declare const figlet: any;
-declare const path: any;
-declare const program: any;
-declare const nodefetch: any;
-declare const LATEST = "LATEST";
-declare function getPackageInfo(packageName: string, packageVersion: string): void;
