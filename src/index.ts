6df383d87ccbe65be660f07fcc014ce57c8c7959
Oron Kastel-Mizrahi <oron.kastel@gmail.com>
2019-12-24 10:51:02 +0200

    pushing some work

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
 const nodefetch = require('node-fetch');
+const semver = require('semver');
 
 const LATEST = 'LATEST';
 
@@ -41,7 +42,10 @@ else{
     versionToDownload = program.ver === undefined ? LATEST : program.ver;
 
     console.log('Downloading ' + program.package + '@' + versionToDownload + ' and its dependencies...');
-    getPackageInfo(program.package,program.ver);
+
+    
+
+    getPackageInfo(program.package,versionToDownload);
 }
 
 
@@ -49,7 +53,7 @@ else{
 //  ** Utils **
 //  ***********
 function getPackageInfo(packageName: string, packageVersion: string) {
-    let url = "https://registry.npmjs.org/debug";
+    let url = "https://registry.npmjs.org/" + packageName;
 
     let settings = { method: "Get" };
 
@@ -58,10 +62,50 @@ function getPackageInfo(packageName: string, packageVersion: string) {
         .then(res => res.json())
         .then((json) => {
 
-            console.log(json.description);
             if(packageVersion===LATEST){
-                packageVersion = json.get('dist-tags').latest;
+                packageVersion = json['dist-tags'].latest;
             }
-            console.log(json.versions[packageVersion].version);
+            let versionInfo = parseVersion(json, packageVersion);
+
+            console.log('processing %s@%s... getting version %s', json.name,packageVersion,versionInfo.version);
+
+            //Handle package depedencies
+            HandleDependencies(versionInfo.dependencies); 
+            
+            //Handle package devDependencies
+            HandleDependencies(versionInfo.devDependencies); 
+ 
+        }).catch( (ex)=> {
+            console.log(chalk.red("ERR! " + ex));
+        }
+            
+        );
+}
+
+
+//Parse the version string to get the apporiate real version.
+//For example, ~version “Approximately equivalent to version”, will update you to the next patch version. ~1.2.3 will use releases from 1.2.3 to <1.3.0.
+//Another example: ^version Will update you to the next minor version .will use releases up to 3.0.0.
+//See: https://docs.npmjs.com/files/package.json
+//Also see: https://docs.npmjs.com/misc/semver and https://docs.npmjs.com/about-semantic-versioning and https://www.npmjs.com/package/semantic-release
+function parseVersion(json: any, packageVersion: string) {
+    var allVersions = new Array();
+
+    Object.keys(json.versions).forEach(ver => {
+        allVersions.push(ver);
+    });
+    
+
+    let parsedVersion = semver.maxSatisfying(allVersions,packageVersion);
+    return json.versions[parsedVersion];
+}
+
+//Iterate a package dependecies in order to handle their dependencies.
+function HandleDependencies(dependencies: any) {
+    if (dependencies !== undefined) {
+        Object.keys(dependencies).forEach(dep => {
+            //!!!!!! TODO: only if the package not already handled before... !!!!!!
+            getPackageInfo(dep, dependencies[dep]);
         });
+    }
 }
